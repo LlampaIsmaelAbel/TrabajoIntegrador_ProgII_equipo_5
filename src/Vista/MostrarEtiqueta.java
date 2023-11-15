68de709be2e7ea8001cfa75352e511b82ff93e03
Ismael_Ll <illampa050@gmail.com>
2023-11-15 19:02:47 -0300

    no message

--- src/Vista/MostrarEtiqueta.java
+++ src/Vista/MostrarEtiqueta.java
@@ -20,7 +20,7 @@ public class MostrarEtiqueta extends JFrame {
 	private void iniciarComponentes() {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 }
