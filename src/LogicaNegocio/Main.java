6edb3cb05c2c15664183406eb8c1239adc11422e
Ismael_Ll <illampa050@gmail.com>
2023-11-15 11:49:07 -0300

    no message

--- src/LogicaNegocio/Main.java
+++ src/LogicaNegocio/Main.java
@@ -1,9 +1,13 @@
 package LogicaNegocio;
 
+import Vista.Principal;;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Principal ventana1= new Principal();
+		ventana1.setVisible(true);
 
 	}
 
--- src/Vista/Principal.java
+++ src/Vista/Principal.java
@@ -7,44 +7,49 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
-	private JDateChooser calendario;// este es unn mesaje del cambio
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				
-				try {
-					Principal frame = new Principal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	
+	
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 818, 483);
+		iniciarComponentes();
+		
+	}
+	
+	private void iniciarComponentes() {
+		
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		calendario = new JDateChooser();
-		calendario.setBounds(150, 87, 87, 20);
-		contentPane.add(calendario);
+		JButton btnNewRefrigerado = new JButton("New button");
+		btnNewRefrigerado.setBounds(134, 88, 124, 41);
+		contentPane.add(btnNewRefrigerado);
+		
+		JButton btnMostrarDatos = new JButton("New button");
+		btnMostrarDatos.setBounds(550, 96, 124, 41);
+		contentPane.add(btnMostrarDatos);
+		
+		JButton btnNewCongelado = new JButton("New button");
+		btnNewCongelado.setBounds(134, 274, 124, 41);
+		contentPane.add(btnNewCongelado);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(550, 274, 124, 41);
+		contentPane.add(btnNewButton_3);
+		
 		
 	}
 }
