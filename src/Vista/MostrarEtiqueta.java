28851a0770e1d72c63640b04edfdf4560f0a9f82
Ismael_Ll <illampa050@gmail.com>
2023-11-15 19:01:40 -0300

    no message

--- src/Vista/MostrarEtiqueta.java
+++ src/Vista/MostrarEtiqueta.java
@@ -7,22 +7,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class ventanaProductoCongelado extends JFrame {
+public class MostrarEtiqueta extends JFrame {
 
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	public ventanaProductoCongelado() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public MostrarEtiqueta() {
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		iniciarComandos();
-
+		iniciarComponentes();
 	}
-	private void iniciarComandos() {
+	
+	private void iniciarComponentes() {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 	}
-
 }
