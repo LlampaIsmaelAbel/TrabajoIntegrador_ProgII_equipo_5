da7f1f74bde9059cc4c3ed0ad0a7b6024af93615
Ismael_Ll <illampa050@gmail.com>
2023-11-14 22:56:26 -0300

    no message

--- src/Vista/Principal.java
+++ src/Vista/Principal.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import com.toedter.calendar.JDateChooser;
 
 public class Principal extends JFrame {
 
@@ -35,8 +36,11 @@ public class Principal extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(149, 89, 87, 20);
+		contentPane.add(dateChooser);
 	}
-
 }
