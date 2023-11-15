67752e64f1540cdb7af04b77ef85caf379f31c9d
Jorgini12 <jorge012joel@gmail.com>
2023-11-15 18:50:18 -0300

    finoli

---
+++ src/Vista/ProductoCongelado.java
@@ -0,0 +1,28 @@
+package Vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ProductoCongelado extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public ProductoCongelado() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		iniciarComponentes();
+		
+	}
+
+	private void iniciarComponentes() {
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+}
