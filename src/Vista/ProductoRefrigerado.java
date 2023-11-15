0c3acb0ab3b0577ede5b4f46b7f7722fe2f646fd
ValentinaCulcuy <valentina.culcuy@gmail.com>
2023-11-15 11:39:55 -0300

    no message

---
+++ src/Vista/ProductoRefrigerado.java
@@ -0,0 +1,29 @@
+package Vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ProductoRefrigerado extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	public ProductoRefrigerado() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 550, 371);
+		iniciarComponentes();
+	}
+	
+	private void iniciarComponentes () {
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
