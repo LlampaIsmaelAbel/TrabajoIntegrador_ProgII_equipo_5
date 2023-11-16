71a1f93acf2d05eba246cf1ee57da7cb6ad63118
Ismael_Ll <illampa050@gmail.com>
2023-11-16 04:16:38 -0300

    no message

--- src/Vista/ventanaProductoCongelado.java
+++ src/Vista/ventanaProductoCongelado.java
@@ -19,7 +19,7 @@ import javax.swing.JSpinner;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class VentanaProductoCongelado extends JFrame {
+public class ProductoCongelado extends JFrame {
 	
 	private ButtonGroup grupRadioBut;
 	private Principal ventPrincipal;
@@ -60,7 +60,7 @@ public class VentanaProductoCongelado extends JFrame {
 	private JSpinner spnTiempo;
 
 
-	public VentanaProductoCongelado() {
+	public ProductoCongelado() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 797, 476);
 		iniciarComponentes();
