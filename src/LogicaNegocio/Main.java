a7b09bae489f87eff6766d450f0001a5f9c574f0
ValentinaCulcuy <valentina.culcuy@gmail.com>
2023-11-20 20:01:58 -0300

    no message

--- src/LogicaNegocio/Main.java
+++ src/LogicaNegocio/Main.java
@@ -28,19 +28,53 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Menu de opciones");
-		System.out.println("1. Grabar un Nuevo Producto");
-		System.out.println("2. Modificar los datos de un Producto determinado");
-		System.out.println("3. Eliminar un Producto determinado");
-		System.out.println("4. Costo Total en Stock");
-		System.out.println("5. Costo Total de un lote determinado");
-		System.out.println("6. Margen de Ganancia total por cada lote");
-		System.out.println("7. Cantidad de productos en cada lote");
-		System.out.println("8. Cantidad de productos de un determinado tipo en cada lote");
-		System.out.println("9. Verificar y eliminar productos vencidos en un lote");
-		System.out.println("10. Mostrar la etiqueta del producto");
 		
+		int opcion = scanner.nextInt();
 		
+		do {
+			System.out.println("Menu de opciones");
+			System.out.println("1. Grabar un Nuevo Producto");
+			System.out.println("2. Modificar los datos de un Producto determinado");
+			System.out.println("3. Eliminar un Producto determinado");
+			System.out.println("4. Costo Total en Stock");
+			System.out.println("5. Costo Total de un lote determinado");
+			System.out.println("6. Margen de Ganancia total por cada lote");
+			System.out.println("7. Cantidad de productos en cada lote");
+			System.out.println("8. Cantidad de productos de un determinado tipo en cada lote");
+			System.out.println("9. Verificar y eliminar productos vencidos en un lote");
+			System.out.println("10. Mostrar la etiqueta del producto");
+			System.out.println("11. Salir");
+			
+			System.out.println("Ingrese la opcion");
+			opcion = scanner.nextInt();
+			switch (opcion) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			case 9:
+				break;
+			case 10:
+				break;
+			case 11:
+				break;
+			default:
+				System.out.println("Opcion invalida");
+			
+			}
+		}while(opcion!=11);
 
 	}
 	
